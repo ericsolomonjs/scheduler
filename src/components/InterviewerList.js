61db61a16f02c822053daadea43ee65a2b210b67
Eric Solomon <eric.solomon.ca@gmail.com>
2023-01-10 09:20:40 -0500

    refactor interviewerlist and interviewerlistitem to not require the id to be passed down anymore

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,7 +5,6 @@ import "components/InterviewerList.scss";
 export default function InterviewerList(props) {
   const interviewerArray = props.interviewers.map((interviewer) => 
     <InterviewerListItem
-      key={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
       selected={interviewer.id === props.interviewer}
