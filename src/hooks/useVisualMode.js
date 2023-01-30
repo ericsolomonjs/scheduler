6a8c090a8c9ee20cccd1907c97eebf3129cf3fe6
Eric Solomon <eric.solomon.ca@gmail.com>
2023-01-30 15:56:42 -0500

    update fns to use prev state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,28 +1,39 @@
 import { useState } from 'react';
 
 export  function useVisualMode(initialMode) {
+
+  //initialize states
   const [mode, setMode] = useState(initialMode);
   const [history, setHistory] = useState([initialMode]);
 
+  //transition fn used to switch between VisualModes
   function transition(changedMode, replace = false) {
-    if (!replace) {
-      setHistory(prev => [...prev, changedMode])
+
+      if (!replace) {
+        setHistory((prev) => [...prev, changedMode])
+      } else {
+        setHistory((prev) => {
+          const tempHistory = [...prev];
+          tempHistory[tempHistory.length-1] = changedMode;
+          return tempHistory;
+        })
+      }
+
       setMode(changedMode);
-    } else {
-      const tempHistory = [...history];
-      tempHistory.pop();
-      tempHistory.push(changedMode);
-      setHistory([...tempHistory])
-      setMode(changedMode);
-    }
+      
   }
 
+  //back function to return to previous History state
   function back() {
     if (history.length > 1) {
-      const tempHistory = [...history];
-      tempHistory.pop();
-      setHistory([...tempHistory]);
-      setMode(tempHistory[tempHistory.length - 1]);
+
+      setHistory((prev) => {
+        const tempHistory = [...prev];
+        tempHistory.pop();
+        setMode(tempHistory[tempHistory.length-1]);
+        return tempHistory;
+      })
+
 
     }
   }
